b6e7bda6f3057ca994d7c7cafc0b043c0cf27277
Francisco Esquibel <f.esquibelalvarez@gmail.com>
2019-09-12 16:04:20 -0300

    minimun edges cost of each node saved

--- script.py
+++ script.py
@@ -1,3 +1,5 @@
+import math
+
 print("Hello A*")
 class Node:
     def __init__(self, id, x, y):
@@ -8,18 +10,49 @@ class Node:
         self.x = x
         self.y = y
         self.parent = 0
+        self.visited = False
+
+
+def EuclideanDistance(node1, node2):
+    return math.sqrt((node2.x - node1.x)**2 + (node2.y - node1.y)**2)
 
 f = open("test.txt", "r")
 
 content = f.readlines()
 
-print(content[0])
-print(content[1])
-
 nodes = []
+nodesCant = 0
 for i in range(1, len(content)):
     line = content[i].split()
-    nodes.append(Node(line[0], line[1], line[2]))
+    nodes.append(Node(int(line[0]), int(line[1]), int(line[2])))
+    nodesCant += 1
 
-for node in nodes:
-    print(node.id)
+print(nodesCant)
+
+
+distanceMatrix = [[0 for x in range(nodesCant)] for y in range(nodesCant)]
+largestDistance = 0
+
+for i in range(0, nodesCant):
+    for j in range(i + 1, nodesCant):
+        if i != j:
+            node1 = nodes[i]
+            node2 = nodes[j]
+            eucDis = EuclideanDistance(node1, node2)
+            if largestDistance <= eucDis:
+                largestDistance = eucDis
+            distanceMatrix[i][j] = eucDis
+            distanceMatrix[j][i] = eucDis
+
+minimunEdges = []
+
+for i in range(0, nodesCant):
+    minimunEdges.append([largestDistance, largestDistance])
+    for j in range(0, nodesCant):
+        if i != j:
+            if minimunEdges[i][0] >= distanceMatrix[i][j]:
+                minimunEdges[i][1] = minimunEdges[i][0]
+                minimunEdges[i][0] = distanceMatrix[i][j]
+
+for i in range(0, len(minimunEdges)):
+    print(i, minimunEdges[i][0], minimunEdges[i][1])
